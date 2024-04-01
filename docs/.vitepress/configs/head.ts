8f4243410f218978e1174fe63259e1ed853b2474
Yang Zeng <40010177+acgtap@users.noreply.github.com>
2024-04-01 22:06:32 +0800

    ok了

--- docs/.vitepress/configs/head.ts
+++ docs/.vitepress/configs/head.ts
@@ -3,7 +3,13 @@ import type { HeadConfig } from 'vitepress'
 const isDevelopment = process.env.NODE_ENV === 'development'
 
 export const head: HeadConfig[] = [
-  ['meta', { name: 'keywords', content: '虚拟局域网,异地组网,免费联机工具,远程联机,局域网联机,游戏联机,MC联机'}],
+  [
+    'meta',
+    {
+      name: 'keywords',
+      content: '虚拟局域网,异地组网,免费联机工具,远程联机,局域网联机,游戏联机,MC联机'
+    }
+  ],
   ['meta', { name: 'theme-color', content: '#ffffff' }],
   ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
   ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
@@ -14,16 +20,39 @@ export const head: HeadConfig[] = [
   ['link', { rel: 'mask-icon', href: '/favicon.ico', color: '#3eaf7c' }],
   ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
   ['script', { src: isDevelopment ? '' : '' }],
+  ['script', { async: '', src: 'https://www.googletagmanager.com/gtag/js?id=G-L6CYJ04S3H' }],
+  ['script', { src: 'https://unpkg.com/sweetalert/dist/sweetalert.min.js' }],
   [
-      'script',
-      { async: '', src: 'https://www.googletagmanager.com/gtag/js?id=G-L6CYJ04S3H' }
-    ],
-    [
-      'script',
-      {},
-      `window.dataLayer = window.dataLayer || [];
+    'script',
+    {},
+    `window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
       gtag('config', 'TAG_ID');`
-    ]
+  ],
+  [
+    'script',
+    {},
+    `function getQueryParam(name){
+    const params = new URLSearchParams(window.location.search);
+    return params.get(name);
+  }
+  const roomId = getQueryParam('roomid');
+  var roomid=roomId;
+  window.roomid=roomId;
+  
+  if (roomId) {
+    const newUrl = 'yyds://room/'+roomId;
+    window.location.href = newUrl;
+    // swal({
+    //   title: "集合啦！",
+    //   text: "你的好友邀请你加入他的房间，是否立即加入？",
+    //   icon: "success",
+    //   button: "Aww yiss!",
+  
+    // }).then((value) => {
+    //   window.location.href = newUrl;
+    // });
+  }`
+  ]
 ]
---
+++ docs/invite.md
@@ -0,0 +1,1 @@
+<iframe style="width:100%;height:500px;border-width:0px;" src="/css.html"></iframe>
