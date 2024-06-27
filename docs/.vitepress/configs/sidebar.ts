41490c0ff69d7b98ec6f2f97f7ba1d3bd54e273a
Kotomi <40010177+acgtap@users.noreply.github.com>
2024-06-27 14:53:06 +0800

    Update sidebar.ts

--- docs/.vitepress/configs/sidebar.ts
+++ docs/.vitepress/configs/sidebar.ts
@@ -15,7 +15,13 @@ export const sidebar: DefaultTheme.Config['sidebar'] = {
     },{
       text: '常见问题',
       link: '/q/'
-        }
+        },{
+      text: '官群交流',
+      link: 'http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=Efi8sPn6xklr_fUBZoeiazbMqTtJS_Oh&authKey=%2BIUJLJvZTvJMV6IpAUre1hTLdiVOfKBjERPhgOpfb0uVv9Bwd4Bc9%2Bp277u0VUfk&noverify=0&group_code=621320122'
+     },{
+      text: '更新日志',
+      link: '/q/hope'
+     }
       ]
     }
   ],
@@ -33,6 +39,12 @@ export const sidebar: DefaultTheme.Config['sidebar'] = {
     },{
       text: '常见问题',
       link: '/q/'
+     },{
+      text: '官群交流',
+      link: 'http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=Efi8sPn6xklr_fUBZoeiazbMqTtJS_Oh&authKey=%2BIUJLJvZTvJMV6IpAUre1hTLdiVOfKBjERPhgOpfb0uVv9Bwd4Bc9%2Bp277u0VUfk&noverify=0&group_code=621320122'
+     },{
+      text: '更新日志',
+      link: '/q/hope'
      }
       ]
     }
