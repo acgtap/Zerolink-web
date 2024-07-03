e872706016a05052b3efafc051ce8c1b65d40be0
Kotomi <40010177+acgtap@users.noreply.github.com>
2024-07-04 04:08:29 +0800

    更新 nav.ts

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -1,7 +1,8 @@
 import type { DefaultTheme } from 'vitepress'
 
 export const nav: DefaultTheme.Config['nav'] = [
-  { text: '首页', link: '/', activeMatch: '^/' },
+  { text: '首页', link: '/', activeMatch: '^/' },,
+  { text: '快速下载', link: '/download', activeMatch: '^/download' },
   { text: '游戏支持与教程', link: '/nav', activeMatch: '^/nav' },
   { text: '常见问题', link: '/q/index', activeMatch: '^/q/index' },
   { text: '游戏资源', link: 'https://www.cngame.wiki', activeMatch: '^/q/index' },
